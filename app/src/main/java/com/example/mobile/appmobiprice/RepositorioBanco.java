2da875678b7224f6c151b38d1bf342aa33fa04e1
DaniloVibo <danilo_sg_@hotmail.com>
2017-06-16 21:59:41 -0300

    Aplicativo-MobiPrice

--- app/src/main/java/com/example/mobile/appmobiprice/RepositorioBanco.java
+++ app/src/main/java/com/example/mobile/appmobiprice/RepositorioBanco.java
@@ -15,11 +15,20 @@ public class RepositorioBanco {
     this.conexao =conexao;
 }
 
-    public void inserir(Produtos produto){
+    public void inserirProdutos(Produtos produto){
         ContentValues values = new ContentValues();
 
         values.put("NOME", produto.getNome());
         values.put("PRECO", produto.getValor());
+
+        conexao.insertOrThrow("MERCADO", null, values);
+    }
+
+    public void inserirMercados(Mercados mercados){
+        ContentValues values = new ContentValues();
+
+        values.put("NOME", mercados.getNome());
+        values.put("ENDERECO", mercados.getEndereco());
 
         conexao.insertOrThrow("MERCADO", null, values);
     }
